b5db824d0d27165965872538944078b56968cb4c
bma <zafer.bora.manga@reinmedical.com>
2021-12-25 20:51:18 +0100

    Reducers ans search component added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,9 @@
-import React, {useEffect, useContext} from 'react'
+import React, {useContext} from 'react'
 import UserItem from './UserItem'
 import GithubContext from '../../context/github/GithubContext'
 function UserResults() {
-    const {users, fetchUsers, loading}=useContext(GithubContext)
+    const {users, loading}=useContext(GithubContext)
     console.log(users);
-    useEffect(()=>{
-        fetchUsers()
-    },[])
-
-
     if(!loading){
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,19 +1,27 @@
-import { createContext, useState } from "react";
-
+import { createContext, useReducer} from "react";
+import githubReducer from "./GithubReducer";
 const GithubContext=createContext()
 
 export const GithubProvider=({children})=>{
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const initialState={
+        users:[],
+        loading:false
+    }
+    const [state, dispatch]= useReducer(githubReducer, initialState)
+
 
     const fetchUsers=async()=>{
+        setLoading()
         const response=await fetch('https://api.github.com/users')
         const data=await response.json()
-        setUsers(data)
-        setLoading(false)
+        dispatch({
+            type:'GET_USERS',
+            payload:data
+        })
         console.log({data})
     }
-    return <GithubContext.Provider value={{users, loading, fetchUsers}}>
+    const setLoading=()=>dispatch({type:'SET_LOADING'})
+    return <GithubContext.Provider value={{users:state.users, loading:state.loading, fetchUsers}}>
         {children}
     </GithubContext.Provider>
 }
