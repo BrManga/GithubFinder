f577bab2b31795b3359cd148b07a18b3582bd96e
bma <zafer.bora.manga@reinmedical.com>
2021-12-27 22:20:13 +0100

    Editing user profile page -1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext"
 import { AlertProvider } from "./context/alert/AlertContext";
 import Alert from "./components/layout/Alert";
+import User from "./pages/User";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               <Alert/>
               <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/user/:login' element={<User/>} />
                 <Route path='/about' element={<About />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -5,11 +5,12 @@ const GithubContext=createContext()
 export const GithubProvider=({children})=>{ 
     const initialState={
         users:[],
+        user:{},
         loading:false
     }
     const [state, dispatch]= useReducer(githubReducer, initialState)
 
-
+    //Searches for users
     const searchUsers=async(text)=>{
         console.log({text});
         setLoading()
@@ -20,9 +21,29 @@ export const GithubProvider=({children})=>{
             payload:items
         })
     }
+    //Gets single user
+    const getUser=async(login)=>{
+        setLoading()
+        const response=await fetch(`https://api.github.com/users/${login}`,{
+            headers:{
+                Authorization:`token `
+            }
+        })
+        if(response.status===404) window.location='/notfound'
+        else{
+
+            const data=await response.json()
+            dispatch({
+                type:'GET_USER',
+                payload:data
+            })
+        }
+    }
+    // Sets loading to true
     const setLoading=()=>dispatch({type:'SET_LOADING'})
+    // Clears all users that are searched before
     const clearUsers=()=>dispatch({type:'CLEAR_USERS'})
-    return <GithubContext.Provider value={{users:state.users, loading:state.loading, searchUsers,clearUsers}}>
+    return <GithubContext.Provider value={{users:state.users, user:state.user, loading:state.loading, searchUsers,clearUsers, getUser}}>
         {children}
     </GithubContext.Provider>
 }
