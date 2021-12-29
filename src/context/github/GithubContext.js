27aaf2f1cc92757406b911bca6842dcf627a9535
bma <zafer.bora.manga@reinmedical.com>
2021-12-29 13:43:35 +0100

    User information expanded, repo information added

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -6,13 +6,13 @@ export const GithubProvider=({children})=>{
     const initialState={
         users:[],
         user:{},
+        repos:[],
         loading:false
     }
     const [state, dispatch]= useReducer(githubReducer, initialState)
 
     //Searches for users
     const searchUsers=async(text)=>{
-        console.log({text});
         setLoading()
         const response=await fetch(`https://api.github.com/search/users?q=${text}`)
         const {items}=await response.json()
@@ -39,11 +39,28 @@ export const GithubProvider=({children})=>{
             })
         }
     }
+        //Gets user repos
+        const getUserRepos=async(login)=>{
+            const response=await fetch(`https://api.github.com/users/${login}/repos?sort=created&per_page=10`,{
+                headers:{
+                    Authorization:`token `
+                }
+            })
+            if(response.status===404) window.location='/notfound'
+            else{
+    
+                const data=await response.json()
+                dispatch({
+                    type:'GET_REPOS',
+                    payload:data
+                })
+            }
+        }
     // Sets loading to true
     const setLoading=()=>dispatch({type:'SET_LOADING'})
     // Clears all users that are searched before
     const clearUsers=()=>dispatch({type:'CLEAR_USERS'})
-    return <GithubContext.Provider value={{users:state.users, user:state.user, loading:state.loading, searchUsers,clearUsers, getUser}}>
+    return <GithubContext.Provider value={{users:state.users, user:state.user, repos:state.repos, loading:state.loading, searchUsers,clearUsers, getUser, getUserRepos}}>
         {children}
     </GithubContext.Provider>
 }
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useParams, Link } from 'react-router-dom'
 import { useContext, useEffect } from 'react/cjs/react.development'
+import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
 function User({ match }) {
-    const { user, getUser, loading } = useContext(GithubContext)
+    const { user, getUser, loading, getUserRepos, repos } = useContext(GithubContext)
     const params = useParams()
     useEffect(() => {
         getUser(params.login)
+        getUserRepos(params.login)
     }, [])
-    const { name, type, avatar_url, location, bio, twitter_username, login, html_url, followers, following, public_repos, public_gists, hireable } = user
-    console.log({ user });
+    const { name, type, avatar_url, location, bio, blog, twitter_username, login, html_url, followers, following, public_repos, public_gists, hireable } = user
     if (loading) return <h1>Loading..</h1>
     return (
         <>
@@ -46,10 +47,86 @@ function User({ match }) {
                                 <a href={html_url} target='_blank' rel='noreferrer' className='btn btn-outline'>Visit Github Profile</a>
                             </div>
                         </div>
+                        <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+                            {location &&
+                                <div className='stat'>
+                                    <div className="stat-title text-md">
+                                        Location
+                                    </div>
+                                    <div className="text-lg stat-value">
+                                        {location}
+                                    </div>
+                                </div>}
+                            {blog &&
+                                <div className='stat'>
+                                    <div className="stat-title text-md">
+                                        Website
+                                    </div>
+                                    <div className="text-lg stat-value">
+                                        <a href={`https://${blog}`} target="_blank" rel='noreferrer'>{blog}</a>
+                                    </div>
+                                </div>}
+                            {twitter_username &&
+                                <div className='stat'>
+                                    <div className="stat-title text-md">
+                                        Twitter
+                                    </div>
+                                    <div className="text-lg stat-value">
+                                        <a href={`https://twitter.com/${twitter_username}`} target="_blank" rel='noreferrer'>{twitter_username}</a>
+                                    </div>
+                                </div>}
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaUsers className='text-3xl md:text-5xl' />
+                        </div>
+                        <div className="stat-title pr-5">
+                            Followers
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:zext-4xl">
+                            {followers}
+                        </div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaUserFriends className='text-3xl md:text-5xl' />
+                        </div>
+                        <div className="stat-title pr-5">
+                            Following
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:zext-4xl">
+                            {following}
+                        </div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaCodepen className='text-3xl md:text-5xl' />
+                        </div>
+                        <div className="stat-title pr-5">
+                            Public Repos
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:zext-4xl">
+                            {public_repos}
+                        </div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaStore className='text-3xl md:text-5xl' />
+                        </div>
+                        <div className="stat-title pr-5">
+                            Public Gists
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:zext-4xl">
+                            {public_gists}
+                        </div>
+                    </div> 
+                </div>
+                <RepoList repos={repos}/>
 
+            </div>
         </>
 
     )
